465de7a7b9ec7b65ff0c543ffd6898740d9397a4
karinarashid1 <karinarashid2010@gmail.com>
2016-11-27 10:52:34 -0800

    setup

---
+++ src/project1/Hello.java
@@ -0,0 +1,14 @@
+package project1;
+
+public class Hello{
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World");
+		int number1 = 10;
+		int number2 = 20;
+		int total =number1 + number2;
+		System.out.println(total);
+		
+	}
+
+}
